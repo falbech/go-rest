10af4e4b81df86a0d19867f4673c9bca56676a66
Felipe Martinez Albeche <felipealbeche@hotmail.com>
2023-08-17 17:59:19 -0300

    remove dead code

--- main.go
+++ main.go
@@ -7,12 +7,7 @@ import (
 )
 
 func main() {
-	// models.Personalities = []models.Personality{
-	// 	{Id: 1, Name: "Name 1", History: "History 1"},
-	// 	{Id: 2, Name: "Name 2", History: "History 2"},
-	// }
 	database.ConnectToDB()
 	fmt.Println("Starting server")
 	routes.HandleRequest()
-
 }
